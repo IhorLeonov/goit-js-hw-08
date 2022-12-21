7f47f4bcd10059982d858bb60e3302317e315c1c
Ihor Leonov <ihorleonow@gmail.com>
2022-12-22 01:41:51 +0200

    upd files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,7 @@
 'use strict';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(SimpleLightbox);
