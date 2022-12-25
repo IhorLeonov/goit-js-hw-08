c67b0acef24a496cdab153452495c6800311f1e8
Ihor Leonov <ihorleonow@gmail.com>
2022-12-25 02:39:11 +0200

    add 1-2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,42 @@
-'use strict';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
 
-console.log(SimpleLightbox);
+const galleryBox = document.querySelector('.gallery');
+const imagesMarkup = createGalleryMarkup(galleryItems);
+galleryBox.insertAdjacentHTML('beforeend', imagesMarkup);
+galleryBox.addEventListener('click', onImageClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+  </div>`;
+    })
+    .join('');
+}
+
+function stopLoadingImg(e) {
+  e.preventDefault();
+}
+
+function summonSimpleLightbox() {
+  new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+function onImageClick(e) {
+  stopLoadingImg(e);
+
+  if (e.target.nodeName === 'IMG') {
+    summonSimpleLightbox();
+  }
+}
